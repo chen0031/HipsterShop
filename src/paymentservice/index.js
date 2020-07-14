8f468cc5f84850589be8b846e34acb11022c7c5c
yuxiaoba <444723257@qq.com>
2020-07-14 11:03:11 +0800

    Add opencensus code for payment

--- src/paymentservice/index.js
+++ src/paymentservice/index.js
@@ -16,21 +16,61 @@
 
 'use strict';
 
-require('@google-cloud/profiler').start({
-  serviceContext: {
-    service: 'paymentservice',
-    version: '1.0.0'
-  }
-});
-require('@google-cloud/trace-agent').start();
-require('@google-cloud/debug-agent').start({
-  serviceContext: {
-    service: 'paymentservice',
-    version: 'VERSION'
-  }
+// require('@google-cloud/profiler').start({
+//   serviceContext: {
+//     service: 'paymentservice',
+//     version: '1.0.0'
+//   }
+// });
+// require('@google-cloud/trace-agent').start();
+// require('@google-cloud/debug-agent').start({
+//   serviceContext: {
+//     service: 'paymentservice',
+//     version: 'VERSION'
+//   }
+// });
+const path = require('path');
+const {logger} = require("@opencensus/core");
+const tracing = require("@opencensus/nodejs");
+const {plugin} = require("@opencensus/instrumentation-grpc");
+const {ZipkinTraceExporter} = require("@opencensus/exporter-zipkin");
+zipkinUrl = process.env.ZIPKIN_COLLECTOR_URL;
+if (!zipkinUrl || zipkinUrl === "off") {
+  console.log("jaeger exporter not initialized");
+  return undefined;
+}
+
+const exporter = new ZipkinTraceExporter({
+  url: zipkinUrl,
+  serviceName: "paymentservice",
+  tags: [
+    {
+      key: "service",
+      value: "paymentservice",
+    },
+  ],
+  logger: logger.logger("info"),
 });
 
-const path = require('path');
+tracing.registerExporter(exporter).start({
+  samplingRate: 1,
+  logLevel: 1,
+});
+
+const basedir = path.dirname(require.resolve("grpc"));
+const version = require(path.join(basedir, "package.json")).version;
+
+// Enables GRPC plugin: Method that enables the instrumentation patch.
+plugin.enable(
+    grpc,
+    tracing.tracer,
+    version,
+    /** plugin options */ {},
+    basedir
+);
+
+console.log("jaeger tracing initialized");
+
 const HipsterShopServer = require('./server');
 
 const PORT = process.env['PORT'];
